f9d40b89a3cda24c1c3822ffde253d3b9676b689
Smurf72T <myskkk@yandex.ru>
2026-05-23 19:50:40 +0500

    Перенёс в .env, читаем через os.getenv('SECRET_KEY')

--- django-admin-panel/admin_panel/settings.py
+++ django-admin-panel/admin_panel/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-dqdg4vt5=!c_=xwtcx)ocd1r&-t$53sod^y)620s&8rq0dqgvl"
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-change-me-in-production")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
