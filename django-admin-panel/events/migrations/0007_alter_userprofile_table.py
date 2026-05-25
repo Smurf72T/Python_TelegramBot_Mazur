9d2107fdb50eb2a3baef03b4bbb0b24294b94a2e
Smurf72T <myskkk@yandex.ru>
2026-05-25 22:55:40 +0500

    Исправление миграций и модели UserProfile

---
+++ django-admin-panel/events/migrations/0007_alter_userprofile_table.py
@@ -0,0 +1,14 @@
+# Generated migration - table already exists as events_userprofile
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('events', '0006_alter_appointment_options_alter_event_options_and_more'),
+    ]
+
+    operations = [
+        # Миграция пустая - таблица уже существует как events_userprofile
+        # Модель UserProfile теперь использует правильную таблицу
+    ]
